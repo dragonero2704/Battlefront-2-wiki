cd2f5b60feb2b033eb08046107b3a9ecb5474a73
dragonero2704 <60794906+dragonero2704@users.noreply.github.com>
2021-04-20 09:50:01 +0200

    better responsive menu

--- function.js
+++ function.js
@@ -1,4 +1,7 @@
-function showhide(){
+function showhide() {
     let menu = document.getElementsByClassName("menu")[0];
-    menu.classList.toggle("menu-show")
+    let hero_content = document.getElementsByClassName("hero__content")[0];
+
+    menu.classList.toggle("menu-show");
+    hero_content.classList.toggle("hide-hero__content")
 }
