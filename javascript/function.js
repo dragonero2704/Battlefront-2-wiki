16f1e12b67593dbd676a66ec6705c5482bcc4d28
Roberto Rudi <roberto.rudi04@gmail.com>
2021-05-04 17:53:35 +0200

    better menu

--- javascript/function.js
+++ javascript/function.js
@@ -6,12 +6,24 @@ function showhide() {
     hero_content.classList.toggle("hide-hero__content")
 }
 
-function header_hover() {
-    let h_container = document.getElementsByClassName("header__container ")[0];
-    h_container.classList.add('b-g')
-}
+// function header_hover() {
+//     let h_container = document.getElementsByClassName("header__container ")[0];
+//     h_container.classList.add('b-g')
+// }
 
-function header_hover_remove() {
-    let h_container = document.getElementsByClassName("header__container ")[0];
-    h_container.classList.remove('b-g')
+// function header_hover_remove() {
+//     let h_container = document.getElementsByClassName("header__container ")[0];
+//     h_container.classList.remove('b-g')
+// }
+
+function black_band() {
+    // console.log('La funzione è stata chiamata')
+    let h_container = document.getElementsByClassName("header__container")[0];
+    if (window.scrollY > window.innerHeight - 100) {
+        console.log('La pagina è stata scrollata')
+        h_container.classList.add('b-g')
+    } else {
+        h_container.classList.remove('b-g')
+    }
+
 }
